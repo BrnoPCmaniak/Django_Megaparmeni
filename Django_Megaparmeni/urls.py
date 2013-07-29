ef036f19efe59f3e0a9a9557a092ff2acd665621
BrnoPCmaniak <brnopcman@seznam.cz>
2013-07-29 23:02:24 +0200

    Moved forum from root to forum/...

--- Django_Megaparmeni/urls.py
+++ Django_Megaparmeni/urls.py
@@ -15,5 +15,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('social_auth.urls')),
-    url(r'', include('djangobb_forum.urls')),
+    url(r'^forum/', include('djangobb_forum.urls')),
 )
